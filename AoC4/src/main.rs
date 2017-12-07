0449d0a87681e05df1d9113f1e76cadd9fc1f421
Paul Schroeder <paulschroeder90@gmail.com>
2017-12-06 19:02:03 -0700

    Aoc4 part2

--- AoC4/src/main.rs
+++ AoC4/src/main.rs
@@ -15,6 +15,23 @@ fn valid_passphrase(input: &str) -> bool
     true
 }
 
+fn valid_passphrase2(input: &str) -> bool
+{
+    let mut unique_strs = HashSet::new();
+    for str in input.split(|c: char| c.is_whitespace())
+    {
+        let mut chars: Vec<char> = str.chars().collect();
+        chars.sort();
+
+        let sorted_str: String = chars.into_iter().collect();
+        if !unique_strs.insert(sorted_str)
+        {
+            return false;     
+        }
+    }
+    true
+}
+
 fn main()
 {
     let mut buffer = String::new(); 
@@ -30,6 +47,13 @@ fn main()
                           .filter(|&vp| vp)
                           .count();
     println!("Number of valid passphrases: {}", num_vp);
+
+    let num_vp2 = input_str.split("\n")
+                          .filter(|&s| !s.is_empty())
+                          .map(&valid_passphrase2)
+                          .filter(|&vp| vp)
+                          .count();
+    println!("Number of valid passphrases 2: {}", num_vp2);
 }
 
 #[cfg(test)]
@@ -56,6 +80,39 @@ mod test
     {
         let ex_str = "aa bb cc dd aaa";
         assert!(valid_passphrase(&ex_str));
+    }
+
+    use valid_passphrase2;
+    #[test]
+    fn example4()
+    {
+        let ex_str = "abcde fghij";
+        assert!(valid_passphrase2(&ex_str));
+    }
+
+    #[test]
+    fn example5()
+    {
+        let ex_str = "abcde xyz ecdab";
+        assert!(!valid_passphrase2(&ex_str));
+    }
+    #[test]
+    fn example6()
+    {
+        let ex_str = "a ab abc abd abf abj";
+        assert!(valid_passphrase2(&ex_str));
+    }
+    #[test]
+    fn example7()
+    {
+        let ex_str = "iiii oiii ooii oooi oooo";
+        assert!(valid_passphrase2(&ex_str));
+    }
+    #[test]
+    fn example8()
+    {
+        let ex_str = "oiii ioii iioi iiio";
+        assert!(!valid_passphrase2(&ex_str));
     }
 
 }
