853b94ab10ac801926e5e997c7cc14e79e5a8fb5
Paul Schroeder <paulschroeder90@gmail.com>
2017-12-06 17:51:34 -0700

    Aoc2 part2

--- AoC2/src/main.rs
+++ AoC2/src/main.rs
@@ -2,8 +2,9 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::str::FromStr;
 use std::env;
+use std::cmp;
 
-fn checksum(input_str: String) -> i32
+fn checksum(input_str: &String) -> i32
 {
     let mut line_checksums: Vec<i32> = Vec::new();
     for line in input_str.lines()
@@ -25,6 +26,31 @@ fn checksum(input_str: String) -> i32
     sum
 }
 
+fn row_divisors(input_str: &String) -> i32
+{
+    let mut line_divisors: Vec<i32> = Vec::new();
+    for line in input_str.lines()
+    {
+        let line_digits: Vec<i32> = line.split(char::is_whitespace)
+                                        .map(&i32::from_str)
+                                        .map(&std::result::Result::unwrap)
+                                        .collect();
+        
+        let a = 
+            (0..line_digits.len()-1).flat_map(|i| line_digits[i+1..].into_iter()
+                                                                    .map(|j| (line_digits[i], j)).collect::<Vec<_>>())
+            .filter(|&(a, b)| cmp::max(a, *b) % cmp::min(a, *b)== 0)
+            .map(|(a, b)| cmp::max(a, *b) / cmp::min(a, *b))
+            .fold(0, |acc, x| acc + x);
+        line_divisors.push(a)
+    }
+    
+    let sum = line_divisors.iter()
+                           .fold(0, |acc, &x| acc + x);
+
+    sum
+}
+
 fn main() 
 {
     let args: Vec<_> = env::args().collect();
@@ -38,14 +64,17 @@ fn main()
     let mut file_str = String::new();
     file.read_to_string(&mut file_str).expect("Error reading file");
 
-    let cs = checksum(file_str);
+    let cs = checksum(&file_str);
     println!("Checksum: {}", cs);
+    let ds = row_divisors(&file_str);
+    println!("Divisorsum: {}", ds);
 }
 
 #[cfg(test)]
 mod test
 {
     use checksum;
+    use row_divisors;
 
     #[test]
     fn provided_example()
@@ -53,6 +82,15 @@ mod test
         let example: String = "5 1 9 5\n\
                                7 5 3\n\
                                2 4 6 8".into();
-        assert!(checksum(example) == 18);
+        assert!(checksum(&example) == 18);
+    }
+
+    #[test]
+    fn provided_example2()
+    {
+        let example: String = "5 9 2 8\n\
+                               9 4 7 3\n\
+                               3 8 6 5".into();
+        assert!(row_divisors(&example) == 9);
     }
 }
